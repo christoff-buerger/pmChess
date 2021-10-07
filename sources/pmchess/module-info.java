ea3558d85285fc5493bdbba162052dc7678a4f76
Christoff Bürger <Christoff.Buerger@gmail.com>
2021-10-08 00:46:10 +0200

    #3: Added license information.

--- sources/pmchess/module-info.java
+++ sources/pmchess/module-info.java
@@ -1,3 +1,10 @@
+/*
+	This program and the accompanying materials are made available under the
+	terms of the MIT license (X11 license) which accompanies this distribution.
+	
+	Author: Christoff Bürger
+*/
+
 module pmchess {
 	requires java.desktop;
 }
